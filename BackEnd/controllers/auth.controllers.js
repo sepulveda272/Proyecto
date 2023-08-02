fd8d0262fb690ff6d5f77a625f8e8c640adc4634
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-08-02 13:26:50 -0500

    feat: :sparkles: avances del back

---
+++ BackEnd/controllers/auth.controllers.js
@@ -0,0 +1,42 @@
+const {response}= require("express");
+const Usuario = require("../models/Usuario.js");
+const bcryptjs = require("bcryptjs");
+
+const login = async (req,res= response)=>{
+    const {email,password} = req.body;
+    try {
+        const emailExists = await Usuario.findOne({email});
+        
+        if(!emailExists){
+            return res.status(400).json({
+                msg: "el email no existe"
+            })
+        }
+
+        if(emailExists.estado === false){
+            return res.status(404).json({
+                msg: "el usuario no esta activo"
+            })
+        }
+
+        const passwordValid = bcryptjs.compareSync(password,emailExists.password);
+        if(!passwordValid){
+            return res.status(400).json({
+                msg: "el password no es correcta"
+            })
+        }
+
+        res.json({
+            msg: "All good duuuuuude"
+        })
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            msg: "datos insuficientes, contacte a servicio tecnico.. juan"
+        })
+    }
+}
+
+module.exports = {
+    login
+}
---
+++ BackEnd/routes/auth.routes.js
@@ -0,0 +1,14 @@
+const {Router} = require("express");
+const {check} = require("express-validator");
+const {login} = require("../controllers/auth.controllers.js");
+const {validateDocument} = require("../middlewares/validate.documents.js");
+
+const router = Router();
+
+router.post("/login",[
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validateDocument
+] ,login);
+
+module.exports = router;
